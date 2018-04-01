ff600e1cd6dede57f1d38d955a30b954790ab21e
Jeffery Cosby <jcoz1968@hotmail.com>
2018-03-31 20:21:59 -0500

    implement UserStore

---
+++ AspNetIdentityDD1/IdentityUserStore.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetIdentityDD1.Models;
+using System.Threading;
+using System.Data.SqlClient;
+using System.Data.Common;
+using Dapper;
+
+namespace AspNetIdentityDD1
+{
+	public class IdentityUserStore : IUserStore<User>
+	{
+
+		public static DbConnection GetOpenConnection()
+		{
+			var connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=IdentityDemo;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+			connection.Open();
+			return connection;
+		}
+
+		public async Task<IdentityResult> CreateAsync(Models.User user, CancellationToken cancellationToken)
+		{
+			using (var connection = GetOpenConnection())
+			{
+				await connection.ExecuteAsync(
+					"insert into Users(Id, UserName, NormalizedUserName, PasswordHash) " + 
+					"values(@id, @userName, @normalizedUserName, @passwordHash)",
+					new
+					{
+						id = user.Id,
+						userName = user.UserName,
+						normalizedUserName = user.NormalizedUserName,
+						passwordHash = user.PasswordHash
+					}
+				);
+			}
+			return IdentityResult.Success;
+		}
+
+		public Task<IdentityResult> DeleteAsync(Models.User user, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Dispose()
+		{
+		}
+
+		public Task<Models.User> FindByIdAsync(string userId, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Models.User> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<string> GetNormalizedUserNameAsync(Models.User user, CancellationToken cancellationToken)
+		{
+			return Task.FromResult(user.NormalizedUserName);
+		}
+
+		public Task<string> GetUserIdAsync(Models.User user, CancellationToken cancellationToken)
+		{
+			return Task.FromResult(user.Id);
+		}
+
+		public Task<string> GetUserNameAsync(Models.User user, CancellationToken cancellationToken)
+		{
+			return Task.FromResult(user.UserName);
+		}
+
+		public Task SetNormalizedUserNameAsync(Models.User user, string normalizedName, CancellationToken cancellationToken)
+		{
+			user.UserName = normalizedName;
+			return Task.CompletedTask;
+		}
+
+		public Task SetUserNameAsync(Models.User user, string userName, CancellationToken cancellationToken)
+		{
+			user.UserName = userName;
+			return Task.CompletedTask;
+		}
+
+		public Task<IdentityResult> UpdateAsync(Models.User user, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
