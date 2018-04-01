0fb6b9363c6b3ccd972c723c7da99e7012738a7c
Jeffery Cosby <jcoz1968@hotmail.com>
2018-03-31 20:53:58 -0500

    controller register method

--- AspNetIdentityDD1/Controllers/HomeController.cs
+++ AspNetIdentityDD1/Controllers/HomeController.cs
@@ -5,11 +5,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AspNetIdentityDD1.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace AspNetIdentityDD1.Controllers
 {
 	public class HomeController : Controller
 	{
+		private UserManager<User> _userManager;
+
+		public HomeController(UserManager<User> userManager)
+		{
+			_userManager = userManager;
+		}
+
 		public IActionResult Index()
 		{
 			return View();
@@ -38,7 +46,24 @@ namespace AspNetIdentityDD1.Controllers
 		[ValidateAntiForgeryToken]
 		public async Task<IActionResult> Register(RegisterModel model)
 		{
+			if(ModelState.IsValid)
+			{
+				var user = await _userManager.FindByNameAsync(model.UserName);
+
+
+				if(user == null)
+				{
+					user = new User
+					{
+						Id = Guid.NewGuid().ToString(),
+						UserName = model.UserName
+					};
+					var result = await _userManager.CreateAsync(user, model.Password);
+				}
+				return View("Success");
+			}
 			return View();
 		}
 	}
+
 }
