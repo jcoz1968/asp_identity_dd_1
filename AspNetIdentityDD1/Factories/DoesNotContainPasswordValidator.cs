c22d634b0f5b1dc5db66792892537e796a8e45a3
Jeffery Cosby <jcoz1968@hotmail.com>
2018-04-01 21:19:49 -0500

    lockout implement

---
+++ AspNetIdentityDD1/Factories/DoesNotContainPasswordValidator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetIdentityDD1.Factories
+{
+	public class DoesNotContainPasswordValidator<TUser> : IPasswordValidator<TUser> where TUser : class
+	{
+		public DoesNotContainPasswordValidator()
+		{
+
+		}
+		public async Task<IdentityResult> ValidateAsync(UserManager<TUser> manager, TUser user, string password)
+		{
+			var username = await manager.GetUserNameAsync(user);
+			if(username == password)
+			{
+				return IdentityResult.Failed(new IdentityError
+				{
+					Description = "Password cannot contain username"
+				});
+			}
+			if(password.Contains("password"))
+			{
+				return IdentityResult.Failed(new IdentityError
+				{
+					Description = "Password cannot contain the word 'password'"
+				});
+			}
+			return IdentityResult.Success;
+		}
+	}
+}
